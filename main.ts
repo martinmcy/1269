eeda3c6d9c669e70a23aecf89ba43d918c3c0831
martinmcy <104663090+martinmcy@users.noreply.github.com>
2022-05-21 11:14:06 +0800

    1

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+radio.onReceivedString(function (receivedString) {
+	
+})
+radio.setGroup(7)
+basic.forever(function () {
+    if (input.acceleration(Dimension.Y) < -500) {
+        radio.sendNumber(1)
+    } else if (input.acceleration(Dimension.Y) > 500) {
+        radio.sendNumber(2)
+    } else if (input.acceleration(Dimension.X) < -500) {
+        radio.sendNumber(3)
+    } else if (input.acceleration(Dimension.X) > 500) {
+        radio.sendNumber(4)
+    } else {
+        radio.sendNumber(5)
+    }
+})
